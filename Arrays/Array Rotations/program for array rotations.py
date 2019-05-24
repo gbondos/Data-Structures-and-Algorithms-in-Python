0221f7c77fdb34acb2d2d949f1bba97e860ad456
gbondos <gbondos@berea.edu>
2019-05-23 21:25:17 -0400

    Added method 2

--- Arrays/Array Rotations/program for array rotations.py
+++ Arrays/Array Rotations/program for array rotations.py
@@ -22,9 +22,37 @@ def rotate_by_d_elements(d, arr):
     for i in range(d):
         rotate_by_one(arr)
 
+# METHOD 2
+"""
+Method 2 - Store d elements in a temp array and shift rest of the array
+"""
+
+def store_d_elements(d, arr):
+    temp = []
+    for i in range(d):
+        temp.append(arr[i])
+    return temp
+
+def shift_array(d, arr):
+    for i in range(len(arr) - d):
+        arr[i] = arr[d + i]
+    return arr
+
+def rotate_array_method2(d, arr):
+    n = len(arr)
+    temp  = store_d_elements(d, arr)
+    shift = shift_array(d, arr)
+    t = d
+    for i in range(d):
+        shift[n - t] = temp[i]
+        t -= 1
+    print(shift)
+
 def main():
     arr = [1, 2, 3, 4, 5, 6, 7]
     rotate_by_d_elements(2, arr)
     print(arr)
+    arr = [1, 2, 3, 4, 5, 6, 7]
+    rotate_array_method2(2, arr)
 
 main()
